8db34436ff17e5ca489523055a007aae6a025390
Isaac Douglas <isaacdouglas2015@gmail.com>
2020-07-03 17:39:49 -0300

    uso do botao para zerar

--- src/index.js
+++ src/index.js
@@ -4,37 +4,14 @@ const { sleep } = require('rpio');
  
 // configura pinos botão e LED
 BTN = 32;
-LED = 29;
  
 // configura botao como entrada e LED como saida
 rpio.open(BTN, rpio.INPUT, rpio.PULL_UP);
-rpio.open(LED, rpio.OUTPUT, rpio.LOW);
  
 // inicializa variáveis para controle do LED e botao
 var btnState = 0;
 var ledState = 1;
 var btnLock = 0;
-var count = 0;
- 
-// setInterval(function() {
-//    btnState = rpio.read(BTN) // le botao
-    
-//    // trata o estado do botao e controla o LED de acordo
-//    if(btnState == 0 && btnLock == 0) {
-//        ledState = !ledState;
-//        ledState ? rpio.write(LED, rpio.LOW) : rpio.write(LED, rpio.HIGH);    
-//        btnLock = 1;
-
-//        console.log('LED1: ' + (ledState ? '0' : '1')); // escreve estado do LED no console
-//        count += 1;
-//        countLata = 0
-//        recycling(count)
-//    }
- 
-//    if(btnState == 1 && btnLock == 1) {
-//        btnLock = 0
-//    }
-// }, 50);
 
 function recycling(count) {
    const path = `http://35.215.197.50:8181/recycling?count=${count}`
@@ -44,21 +21,35 @@ function recycling(count) {
    });
 }
 
-var limiar = 1000
-var ldrCount = 0
-var countLata = 0
 
 PIN_LDR = 7
 LED_LDR = 23
 
 rpio.open(LED_LDR, rpio.OUTPUT, rpio.LOW)
 
+var limiar = 1000
+var ldrCount = 0
+var countLata = 0
+
 var livreOld = false
 var livreNew = true
-
 var on = true
 
 setInterval(function() {
+   btnState = rpio.read(BTN)
+
+   if(btnState == 0 && btnLock == 0) {
+      ledState = !ledState
+      btnLock = 1
+
+      recycling(countLata)
+      countLata = 0
+   }
+ 
+   if(btnState == 1 && btnLock == 1) {
+       btnLock = 0
+   }
+
    ldrCount = 0
    rpio.open(PIN_LDR, rpio.OUTPUT, rpio.LOW);
    sleep(0.1)
@@ -77,11 +68,13 @@ setInterval(function() {
    }
 
    if (on) {
+      countLata += 1
+      console.log("LATAS: " + countLata);
       rpio.write(LED_LDR, rpio.HIGH)
    } else {
       rpio.write(LED_LDR, rpio.LOW)
    }
    
-   console.log(ldrCount);
+   // console.log(ldrCount);
 }, 10);
 
