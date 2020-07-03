e9bc70743556e2f22a168e7ede050110e44c7342
Isaac Douglas <isaacdouglas2015@gmail.com>
2020-07-02 22:23:17 -0300

    botao

--- src/index.js
+++ src/index.js
@@ -11,3 +11,11 @@ setInterval(function() {
    if(ledState == 0) rpio.write(LED, rpio.HIGH); //acende LED
    else rpio.write(LED, rpio.LOW); //apaga LED
 }, 1000); //configura intervalo de 1000 ms
+
+BTN = 32; //define pino da chave 
+ 
+rpio.open(BTN, rpio.INPUT, rpio.PULL_UP); //configura botao como input
+ 
+setInterval(function() {
+   console.log('Button State: ' + (rpio.read(BTN) ? 'OFF' : 'ON')); //escreve no console estado do botao
+}, 10); //configura intervalo de 10 ms para leitura
